00d664f87ae9b5e2b1ecbd04d9c39ded9dae41ba
Adoniran França <adoniranfranca@gmail.com>
2025-04-19 22:52:23 -0300

    test(auth): add authentication helper and update articles specs

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Articles' do
   before(:each) { Article.create(title: 'art 1', content: 'conteudo do art 1') }
+  let(:header) { http_basic_auth_header }
 
   context 'GET /api/v1/articles' do
     it 'responds with status Ok' do
-      get '/api/v1/articles.json'
+      get '/api/v1/articles.json', headers: header
 
       expect(response).to have_http_status(:ok)
     end
 
     it 'responds with correct persisted article json' do
-      get '/api/v1/articles.json'
+      get '/api/v1/articles.json', headers: header
 
       expect(json_to_hash(response.body)[:data].first).not_to have_key(:created_at)
       expect(json_to_hash(response.body)[:data].first).not_to have_key(:updated_at)
